6a7ebac4d4b3e51a09970cead49d2863fdc6054a
jglorange <jglorange@gmail.com>
2021-05-10 21:26:41 +0000

    meta

--- src/components/templates/Project/Project.tsx
+++ src/components/templates/Project/Project.tsx
@@ -5,20 +5,31 @@ import { projectResolver } from '@src/data/resolvers'
 import Head from '@cmp/site/Head'
 import SliceMapping from '@cmp/slices/sliceMapping'
 import FeaturedImage from '@cmp/site/FeaturedImage'
+import { Helmet } from 'react-helmet'
 
 const Project = ({ data }: { data: any }) => {
   const project = projectResolver(data.prismicProject)
 
   return (
-    <div className='page h-100 m-auto position-relative pt-3 container'>
-      <Head title={project.title.text} description={project.type} />
-      <FeaturedImage {...project.featuredImage} />
-      <div className='d-flex flex-wrap w-100'>
-        {project.body.map((slice, i) => (
-          <SliceMapping key={i} slice={slice} />
-        ))}
+    <>
+      <Helmet>
+        <title>{`${project.title.text} - Þykjó`}</title>
+        <meta property='description' content={project.shortDescription.text} />
+        <meta
+          property='og:description'
+          content={project.shortDescription.text}
+        />
+      </Helmet>
+      <div className='page h-100 m-auto position-relative pt-3 container'>
+        <Head title={project.title.text} description={project.type} />
+        <FeaturedImage {...project.featuredImage} />
+        <div className='d-flex flex-wrap w-100'>
+          {project.body.map((slice, i) => (
+            <SliceMapping key={i} slice={slice} />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
