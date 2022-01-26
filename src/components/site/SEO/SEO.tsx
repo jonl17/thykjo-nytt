b64291b6d5fdb580a1894453b536f604c024a227
Jón Gabríel Lorange <jonl17@ru.is>
2022-01-26 18:28:03 +0000

    feat: SEO update

--- src/components/site/SEO/SEO.tsx
+++ src/components/site/SEO/SEO.tsx
@@ -33,11 +33,19 @@ const SEO = ({ lang }: SEOProps) => {
       <title>{meta.title}</title>
       <link rel='icon' href={meta.favicon.url} type='image/png' />
 
-      <meta property='description' content={meta.description} />
-      <meta property='keywords' content={meta.keywords} />
+      <meta
+        property='description'
+        name='description'
+        content={meta.description}
+      />
+      <meta property='keywords' name='keywords' content={meta.keywords} />
 
-      <meta property='og:image' content={meta.image.url} />
-      <meta property='og:description' content={meta.description} />
+      <meta property='og:image' name='og:image' content={meta.image.url} />
+      <meta
+        property='og:description'
+        name='og:description'
+        content={meta.description}
+      />
       <meta name='twitter:description' content={meta.description} />
       <meta name='twitter:image' content={meta.image.url} />
       <meta name='twitter:card' content='summary_large_image' />
